02ed10e1488a8ad5605722b0926544838a1560bb
Steven Walsh <24912514+stewalsh04@users.noreply.github.com>
2019-07-29 15:36:17 +0100

    reverse_complement.r
    This function will give you the reverse complement of a DNA sequence

---
+++ reverse_complement.r
@@ -0,0 +1,23 @@
+##Function to get reverse complement
+
+rc <- function(z){
+  rc1 <- function(zz){
+    s <- strsplit(zz, split = "")[[1]]
+    s <- rev(s)
+    dchars <- strsplit("ACGTMRWSYKVHDBNI", split = "")[[1]]
+    comps <- strsplit("TGCAKYWSRMBDHVNI", split = "")[[1]]
+    s <- s[s %in% dchars] # remove spaces etc
+    s <- dchars[match(s, comps)]
+    s <- paste0(s, collapse = "")
+    return(s)
+  }
+  z <- toupper(z)
+  tmpnames <- names(z)
+  res <- unname(sapply(z, rc1))
+  if(!is.null(attr(z, "quality"))){
+    strev <- function(x) sapply(lapply(lapply(unname(x), charToRaw), rev), rawToChar)
+    attr(res, "quality") <- unname(sapply(attr(z, "quality"), strev))
+  }
+  names(res) <- tmpnames
+  return(res)
+}
